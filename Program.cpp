6e55f15e847659761809817de07f1fef2bf6480b
YamanaSousuke <103532568+YamanaSousuke@users.noreply.github.com>
2022-12-21 01:58:47 +0900

    [add] Add message loop
    メッセージループの追加

--- Program.cpp
+++ Program.cpp
@@ -1,5 +1,23 @@
 #include <Windows.h>
 
+
+// 受信したメッセージに応じた処理
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		// 終了処理
+		PostQuitMessage(0);
+		return 0;
+
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
 // エントリーポイント
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int nCmdShow)
 {
@@ -10,7 +28,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
 	const wchar_t className[] = L"GameWindow";
 	WNDCLASSEX wndClass = {};
 	wndClass.cbSize = sizeof(WNDCLASSEX);
-	wndClass.lpfnWndProc = DefWindowProc;
+	wndClass.lpfnWndProc = WindowProc;
 	wndClass.hInstance = hInstance;
 	wndClass.lpszClassName = className;
 	if (!RegisterClassEx(&wndClass))
@@ -24,8 +42,8 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
 	AdjustWindowRectEx(&rect, WS_OVERLAPPEDWINDOW, FALSE, 0);
 
 	// ウィンドウの作成
-	hWnd = CreateWindowEx(0, L"Static", L"Title", 
-		CW_USEDEFAULT, CW_USEDEFAULT, WS_OVERLAPPEDWINDOW, 
+	hWnd = CreateWindowEx(0, className, L"Title", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 
 		(rect.right - rect.left), (rect.bottom - rect.top),
 		NULL, NULL, hInstance, NULL);
 	if (hWnd == NULL)
@@ -38,5 +56,20 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
 	ShowWindow(hWnd, nCmdShow);
 	// クライアント領域の更新
 	UpdateWindow(hWnd);
+
+	// メッセージループ
+	MSG msg = {};
+	while (true) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) {
+			// メッセージを取得
+			if (!GetMessage(&msg, NULL, 0, 0)) {
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
 	return 0;
 }
